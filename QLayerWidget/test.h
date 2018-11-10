457a5aa9012a1f6d1e820093809fbc957b91b60c
Ali Ajorian <arastoo.ajorian@gmail.com>
2018-11-10 11:03:53 +0330

    Add files via upload

---
+++ QLayerWidget/test.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_test.h"
+
+struct ItemData 
+{
+	int start = 0, end = 0;
+	QString getName()const { return QString("%1-%2").arg(QString::number(start)).arg(QString::number(end)); }
+};
+
+class test : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    test(QWidget *parent = Q_NULLPTR);
+
+private:
+    Ui::tstClass ui;
+
+private slots:
+	void addItem();
+	void deleteItem();
+};
---
+++ QLayerWidget/test.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-11-10T09:13:01
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = test
+TEMPLATE = app
+
+
+SOURCES += 	main.cpp\
+			QLayerWidget.cpp \
+			test.cpp
+
+HEADERS  += QLayerWidget.h \
+			test.h
+
+FORMS    += test.ui
