82f7c65e0b62d168db371226fcace8c615c06b34
Ali Ajorian <arastoo.ajorian@gmail.com>
2018-11-10 14:54:38 +0330

    Update test.cpp
    adding menu for items

--- QLayerWidget/test.cpp
+++ QLayerWidget/test.cpp
@@ -7,6 +7,23 @@ test::test(QWidget *parent)
 	connect(ui.lineEdit, SIGNAL(textChanged(QString)), ui.layerWidget, SLOT(filter(QString)));
 	connect(ui.btnAdd, SIGNAL(clicked()), this, SLOT(addItem()));
 	connect(ui.btnDelete, SIGNAL(clicked()), this, SLOT(deleteItem()));
+    initActions();
+}
+
+void test::initActions()
+{
+    QVector<QAction*> actions;
+    QAction* bld = new QAction(QIcon(":/icons/bold.png"), tr("&Bold"), this);
+    bld->setStatusTip(tr("Bold Text"));
+    connect(bld, &QAction::triggered, this, [=]()
+    {
+        LayerInfo info = ui.layerWidget->getCurrentLayer();
+        if (info.removable)
+            ui.layerWidget->currentItem()->setTextColor(QColor(255, 0, 0));
+    });
+
+    actions.append(bld);
+    ui.layerWidget->setMenuActions(actions);
 }
 
 void test::addItem()
@@ -21,3 +38,4 @@ void test::deleteItem()
 {
 	ui.layerWidget->deleteLayer();
 }
+
