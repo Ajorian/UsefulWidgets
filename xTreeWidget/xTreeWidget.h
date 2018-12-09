9bdeaccac2851217db5c9a05d778f7d376327594
Ali Ajorian <arastoo.ajorian@gmail.com>
2018-12-09 08:54:49 +0330

    Update xTreeWidget.h
    saveToXML function is added

--- xTreeWidget/xTreeWidget.h
+++ xTreeWidget/xTreeWidget.h
@@ -6,7 +6,12 @@
 
 struct NodeData
 {
-	NodeData(const QString type, const GXP::AtributesVector& fields){}
+    QString type;
+    NodeData(const QString _type, const GXP::AtributesVector& fields):type(_type){}
+	QString toXML()const // you don't have to push '<' 'type' and '>' in the beginning and the end of the return string;
+	{
+        return QString();
+	}
 };
 
 struct NodeInfo
@@ -15,8 +20,11 @@ struct NodeInfo
 	quint16 stateFlags=0;
 	NodeData* data=0;
 
+	static quint16 parseFlags(const QString);
+
 	NodeInfo(){}
 	NodeInfo(const QString type, const GXP::AtributesVector& fields) { data = new NodeData(type, fields); }
+	QString flagsToString()const;
 };
 Q_DECLARE_METATYPE(NodeInfo);
 
@@ -27,14 +35,16 @@ class xTreeWidget : public QWidget
 public:
 	xTreeWidget(QWidget *parent = Q_NULLPTR);
 	~xTreeWidget();
+	void clear() { ui.tree->clear(); }
     void readXML(const QString path);
-    void clear(){ui.tree->clear();}
+	void saveToXML(const QString path)const;
 
 private slots:
     void on_led_search_textChanged(const QString &arg1);
 
 private:
     Ui::xTreeWidget ui;
+	QString rootType;
 	void XMLtoTreeWidget(const GXP::parser& prs);
     void filter(const QString str,const QVector<int> columns);
 };
