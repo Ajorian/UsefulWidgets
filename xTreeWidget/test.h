e8f9410ca378ff9545bcf3694a5fe6e6bb1546b3
Ali Ajorian <arastoo.ajorian@gmail.com>
2018-12-09 14:26:01 +0330

    Update test.h

--- xTreeWidget/test.h
+++ xTreeWidget/test.h
@@ -2,28 +2,6 @@
 #define TEST_H
 
 #include <QMainWindow>
-#include "xTreeWidget.h"
-
-#define XML_NODE_TYPE_PANEL "panel"
-#define FIELD_NAME	"name"
-#define FIELD_TITLE "title"
-
-struct Panel : public NodeData
-{
-    QString name, title;
-    Panel(const GXP::AtributesVector& fields)
-    {
-        for (size_t i=0; i<fields.size(); i++)
-            {
-                if (QString(fields[i].first.c_str()).toLower() == FIELD_NAME)
-                    name = fields[i].second.c_str();
-                else if (QString(fields[i].first.c_str()).toLower() == FIELD_TITLE)
-                    title = fields[i].second.c_str();
-            }
-    }
-
-    QString toString()const{return QString("%1=\"%2\" %3=\"%4\"").arg(FIELD_NAME).arg(name).arg(FIELD_TITLE).arg(title);}
-};
 
 namespace Ui {
 class test;
